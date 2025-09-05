20e6bd329651e292a2100cf3f2f737b24568f941
TianFu <tianfuzhao80@gmail.com>
2025-09-05 16:44:52 +0800

    添加中文新支持

--- Toolkit/ChineseSupport.py
+++ Toolkit/ChineseSupport.py
@@ -2,6 +2,11 @@ import functools
 from pylab import mpl
 
 
+def show_chinese():
+    mpl.rcParams["font.sans-serif"] = ["SimHei"]
+    mpl.rcParams["axes.unicode_minus"] = False
+
+
 def enable_chinese(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
